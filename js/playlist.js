1cf5f4299ba3cf15b5f67d106f14ede87af45581
vjastoni <vjastoni13@gmail.com>
2023-07-18 10:17:26 +0800

    first commit

---
+++ js/playlist.js
@@ -0,0 +1,32 @@
+let songs = [
+  {
+    name: 'Ordinary Song - Elli(Cover Ins)',
+    artist: 'Marc Velasco',
+    img: 'img1',
+    audio: 'ordinary song',
+  },
+  {
+    name: 'Celeste - Ins',
+    artist: 'Tothapi',
+    img: 'img2',
+    audio: 'celeste',
+  },
+  {
+    name: 'Sometimes - Elli(Cover Ins)',
+    artist: 'Britney Spears',
+    img: 'img3',
+    audio: 'sometimes',
+  },
+  {
+    name: 'Drunk - Elli(Cover)',
+    artist: 'Keshi',
+    img: 'img4',
+    audio: 'drunk',
+  },
+  {
+    name: 'Nothing - Elli(Cover)',
+    artist: 'Bruno Major',
+    img: 'img5',
+    audio: 'nothing',
+  },
+];
